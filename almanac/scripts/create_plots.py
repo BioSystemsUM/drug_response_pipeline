0867415def109ca7a95d8c3523623f2874e865ac
Delora Baptista <dlr.sb1@gmail.com>
2022-01-13 15:52:39 +0000

    Changed model names (subnetworks as subscripts)
    Former-commit-id: 02ce1a51ee18c9a5757de397450cae97ec6d277b

--- almanac/scripts/create_plots.py
+++ almanac/scripts/create_plots.py
@@ -3,10 +3,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-
-
 SMALL_SIZE = 18
-MEDIUM_SIZE = 20
+MEDIUM_SIZE = 22
 BIGGER_SIZE = 24
 
 plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
@@ -14,33 +12,33 @@ plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
 plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
 plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
-new_model_names = {'one-hot encoded cell line names + drug (ECFP4, 1024, dense)': 'cell line (one-hot encoded) + drugs (ECFP4)',
-               'one-hot encoded cell line names + one-hot encoded drugs': 'cell line (one-hot encoded) + drugs (one-hot encoded)',
-               'Baseline (always predicts the mean of the training set)': 'baseline',
-               'expr (landmark genes (REDO), dense, MinMaxScaler) + drug (ECFP4, Dense)': 'expr (landmark) + drugs (ECFP4)',
-               'expr (NCG genes, dense, MinMaxScaler) + drug (ECFP4, Dense)': 'expr (NCG) + drugs (ECFP4)',
-               'expr (target genes (all interactions), dense, MinMaxScaler) + drug (ECFP4, Dense)': 'expr (DGI) + drug (ECFP4)',
-               'expr (targets full (all drug-gene interactions) + NCG genes, dense, MinMaxScaler) + drug (ECFP4, Dense)': 'expr (DGI + NCG) + drugs (ECFP4)',
-               'expr (COSMIC genes, dense, MinMaxScaler) + drug (ECFP4, Dense)': 'expr (COSMIC) + drugs (ECFP4)',
-               'expr (targets (all interactions) + landmark genes, dense, MinMaxScaler) + drug (ECFP4, Dense)': 'expr (DGI + landmark) + drugs (ECFP4)',
-               'expr (UMAP) + drug (ECFP4, 1024, dense)': 'expr (UMAP) + drugs (ECFP4)',
-               'expr (WGCNA, dense) + drug (ECFP4 1024, dense)': 'expr (WGCNA) + drugs (ECFP4)',
-               'expr (full, clustering order, 1D Conv) + drug (ECFP4, 1024, dense)': 'expr (protein coding, clustering order 1D CNN) + drugs (ECFP4)',
-               'expr (full, chromosome position order, 1D Conv) + drug (ECFP4, 1024, dense)': 'expr (protein coding, chromosome position order 1D CNN) + drugs (ECFP4)',
-               'expr (full, chromosome position order, 2D Conv) + drug (ECFP4, 1024, dense)': 'expr (protein coding, chromosome position order 2D CNN) + drugs (ECFP4)',
-               'expr (full, clustering order, 2D Conv) + drug (ECFP4, 1024, dense)': 'expr (protein coding, clustering order 2D CNN) + drugs (ECFP4)',
-               'expr (full, dense, MinMaxScaler) + drug (ECFP4, 1024, dense)': 'expr (protein coding) + drugs (ECFP4)',
-               'expr (target genes (all interactions), dense, MinMaxScaler) + drug (MolecularTransformerEmbeddings, Dense)': 'expr (DGI) + drugs (MTE)',
-               'expr (target genes (all interactions), dense, MinMaxScaler) + drug (LayeredFP, Dense)': 'expr (DGI) + drugs (LayeredFP)',
-               'expr (target genes (all interactions), dense, MinMaxScaler) + drug (GCN)': 'expr (DGI) + drugs (GCN)',
-               'expr (target genes (all interactions), dense, MinMaxScaler) + drug (TextCNN)': 'expr (DGI) + drugs (TextCNN)',
-               'expr (target genes, dense, MinMaxScaler) + mut (gene-level, target genes) + cnv (GISTIC, target genes) + drug (ECFP4, Dense)': 'expr (DGI) + mut (DGI, gene-level) + cnv (DGI) + drugs (ECFP4)',
-               'expr (target genes, dense, MinMaxScaler) + mut (pathway-level, target genes) + cnv (GISTIC, target genes) + drug (ECFP4, Dense)': 'expr (DGI) + mut (pathway-level) + cnv (DGI) + drugs (ECFP4)',
-               'expr (target genes (all interactions), dense, MinMaxScaler) + one-hot encoded drugs': 'expr (DGI) + drugs (one-hot encoded)',
-               'expr (target genes (all interactions), dense, MinMaxScaler) + drug (GAT)': 'expr (DGI) + drugs (GAT)',
+new_model_names = {'one-hot encoded cell line names + drug (ECFP4, 1024, dense)': '$cell\ line_{one\ hot} + drugs_{ECFP4}$',
+               'one-hot encoded cell line names + one-hot encoded drugs': '$cell\ line_{one\ hot} + drugs_{one\ hot}$',
+               'Baseline (always predicts the mean of the training set)': '$baseline$',
+               'expr (landmark genes (REDO), dense, MinMaxScaler) + drug (ECFP4, Dense)': '$expr_{landmark} + drugs_{ECFP4}$',
+               'expr (NCG genes, dense, MinMaxScaler) + drug (ECFP4, Dense)': '$expr_{NCG} + drugs_{ECFP4}$',
+               'expr (target genes (all interactions), dense, MinMaxScaler) + drug (ECFP4, Dense)': '$expr_{DGI} + drugs_{ECFP4}$',
+               'expr (targets full (all drug-gene interactions) + NCG genes, dense, MinMaxScaler) + drug (ECFP4, Dense)': '$expr_{DGI + NCG} + drugs_{ECFP4}$',
+               'expr (COSMIC genes, dense, MinMaxScaler) + drug (ECFP4, Dense)': '$expr_{COSMIC} + drugs_{ECFP4}$',
+               'expr (targets (all interactions) + landmark genes, dense, MinMaxScaler) + drug (ECFP4, Dense)': '$expr_{DGI + landmark} + drugs_{ECFP4}$',
+               'expr (UMAP) + drug (ECFP4, 1024, dense)': '$expr_{UMAP} + drugs_{ECFP4}$',
+               'expr (WGCNA, dense) + drug (ECFP4 1024, dense)': '$expr_{WGCNA} + drugs_{ECFP4}$',
+               'expr (full, clustering order, 1D Conv) + drug (ECFP4, 1024, dense)': '$expr_{protein\ coding,\ clustering\ order\ 1D\ CNN} + drugs_{ECFP4}$',
+               'expr (full, chromosome position order, 1D Conv) + drug (ECFP4, 1024, dense)': '$expr_{protein\ coding,\ chromosome\ position\ order\ 1D\ CNN} + drugs_{ECFP4}$',
+               'expr (full, chromosome position order, 2D Conv) + drug (ECFP4, 1024, dense)': '$expr_{protein\ coding,\ chromosome\ position\ order\ 2D\ CNN} + drugs_{ECFP4}$',
+               'expr (full, clustering order, 2D Conv) + drug (ECFP4, 1024, dense)': '$expr_{protein\ coding,\ clustering\ order\ 2D\ CNN} + drugs_{ECFP4}$',
+               'expr (full, dense, MinMaxScaler) + drug (ECFP4, 1024, dense)': '$expr_{protein\ coding} + drugs_{ECFP4}$',
+               'expr (target genes (all interactions), dense, MinMaxScaler) + drug (MolecularTransformerEmbeddings, Dense)': '$expr_{DGI} + drugs_{MTE}$',
+               'expr (target genes (all interactions), dense, MinMaxScaler) + drug (LayeredFP, Dense)': '$expr_{DGI} + drugs_{LayeredFP}$',
+               'expr (target genes (all interactions), dense, MinMaxScaler) + drug (GCN)': '$expr_{DGI} + drugs_{GCN}$',
+               'expr (target genes (all interactions), dense, MinMaxScaler) + drug (TextCNN)': '$expr_{DGI} + drugs_{TextCNN}$',
+               'expr (target genes, dense, MinMaxScaler) + mut (gene-level, target genes) + cnv (GISTIC, target genes) + drug (ECFP4, Dense)': '$expr_{DGI} + mut_{DGI,\ gene-level} + cnv_{DGI} + drugs_{ECFP4}$',
+               'expr (target genes, dense, MinMaxScaler) + mut (pathway-level, target genes) + cnv (GISTIC, target genes) + drug (ECFP4, Dense)': '$expr_{DGI} + mut_{pathway-level} + cnv_{DGI} + drugs_{ECFP4}$',
+               'expr (target genes (all interactions), dense, MinMaxScaler) + one-hot encoded drugs': '$expr_{DGI} + drugs_{one\ hot}$',
+               'expr (target genes (all interactions), dense, MinMaxScaler) + drug (GAT)': '$expr_{DGI} + drugs_{GAT}$',
                'ElasticNet - expr (target genes (all interactions)) + drug (ECFP4)': 'ElasticNet',
                'XGBoost - expr (target genes (all interactions)) + drug (ECFP4)': 'XGBoost',
                'Random Forest - expr (target genes (all interactions)) + drug (ECFP4)': 'Random Forest',
@@ -65,24 +63,23 @@ ml_results.rename(columns=metrics_aliases, inplace=True)
 ml_results['model'].replace(new_model_names, inplace=True)
 
 # Plots comparing different expr preprocessing steps/subnetworks
-diff_expr_models = ['expr (protein coding) + drugs (ECFP4)',
-					'expr (protein coding, chromosome position order 1D CNN) + drugs (ECFP4)',
-					'expr (protein coding, clustering order 1D CNN) + drugs (ECFP4)',
-					'expr (protein coding, chromosome position order 2D CNN) + drugs (ECFP4)',
-					'expr (protein coding, clustering order 2D CNN) + drugs (ECFP4)',
-					'expr (landmark) + drugs (ECFP4)',
-					'expr (DGI) + drug (ECFP4)',
-					'expr (COSMIC) + drugs (ECFP4)',
-					'expr (NCG) + drugs (ECFP4)',
-					'expr (DGI + landmark) + drugs (ECFP4)',
-					'expr (DGI + NCG) + drugs (ECFP4)',
-					'expr (UMAP) + drugs (ECFP4)',
-					'expr (WGCNA) + drugs (ECFP4)',
-					'cell line (one-hot encoded) + drugs (ECFP4)',
-					'cell line (one-hot encoded) + drugs (one-hot encoded)',
-					'baseline']
+diff_expr_models = ['$expr_{protein\ coding} + drugs_{ECFP4}$',
+					'$expr_{protein\ coding,\ chromosome\ position\ order\ 1D\ CNN} + drugs_{ECFP4}$',
+					'$expr_{protein\ coding,\ clustering\ order\ 1D\ CNN} + drugs_{ECFP4}$',
+					'$expr_{protein\ coding,\ chromosome\ position\ order\ 2D\ CNN} + drugs_{ECFP4}$',
+					'$expr_{protein\ coding,\ clustering\ order\ 2D\ CNN} + drugs_{ECFP4}$',
+                    '$expr_{landmark} + drugs_{ECFP4}$',
+                    '$expr_{DGI} + drugs_{ECFP4}$',
+					'$expr_{COSMIC} + drugs_{ECFP4}$',
+                    '$expr_{NCG} + drugs_{ECFP4}$',
+					'$expr_{DGI + landmark} + drugs_{ECFP4}$',
+                    '$expr_{DGI + NCG} + drugs_{ECFP4}$',
+					'$expr_{UMAP} + drugs_{ECFP4}$',
+					'$expr_{WGCNA} + drugs_{ECFP4}$',
+                    '$cell\ line_{one\ hot} + drugs_{ECFP4}$',
+                    '$cell\ line_{one\ hot} + drugs_{one\ hot}$',
+                    '$baseline$']
 results = dl_results[dl_results['model'].isin(diff_expr_models)]
-
 
 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(16, 16))
 ax = [ax1, ax2, ax3, ax4]
@@ -109,16 +106,15 @@ plt.subplots_adjust(bottom=0.05, wspace=0.4)
 fig.savefig('../results/different_expression_models.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
 fig.savefig('../results/different_expression_models.pdf', bbox_extra_artists=(lgd,), bbox_inches='tight')
 
-# Plots comparing different drug subnetworks
-diff_drug_models = ['expr (DGI) + drug (ECFP4)',
-					'expr (DGI) + drugs (LayeredFP)',
-					'expr (DGI) + drugs (TextCNN)',
-					'expr (DGI) + drugs (GCN)',
-					'expr (DGI) + drugs (GAT)',
-					'expr (DGI) + drugs (MTE)',
-					'expr (DGI) + drugs (one-hot encoded)',
-					'cell line (one-hot encoded) + drugs (one-hot encoded)',
-					'baseline']
+diff_drug_models = ['$expr_{DGI} + drugs_{ECFP4}$',
+					'$expr_{DGI} + drugs_{LayeredFP}$',
+					'$expr_{DGI} + drugs_{TextCNN}$',
+					'$expr_{DGI} + drugs_{GCN}$',
+					'$expr_{DGI} + drugs_{GAT}$',
+					'$expr_{DGI} + drugs_{MTE}$',
+					'$expr_{DGI} + drugs_{one\ hot}$',
+                    '$cell\ line_{one\ hot} + drugs_{one\ hot}$',
+					'$baseline$']
 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(16, 16))
 ax = [ax1, ax2, ax3, ax4]
 for i, metric in enumerate(set(metrics_aliases.values())):
@@ -145,9 +141,9 @@ fig.savefig('../results/different_drug_models.pdf', bbox_extra_artists=(lgd,), b
 
 
 # Plots comparing expr+drugs and expr+mut+cnv+drugs
-extra_omics_models = ['expr (DGI) + drug (ECFP4)',
-					  'expr (DGI) + mut (DGI, gene-level) + cnv (DGI) + drugs (ECFP4)',
-					  'expr (DGI) + mut (pathway-level) + cnv (DGI) + drugs (ECFP4)']
+extra_omics_models = ['$expr_{DGI} + drugs_{ECFP4}$',
+                      '$expr_{DGI} + mut_{DGI,\ gene-level} + cnv_{DGI} + drugs_{ECFP4}$',
+					  '$expr_{DGI} + mut_{pathway-level} + cnv_{DGI} + drugs_{ECFP4}$']
 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(16, 16))
 ax = [ax1, ax2, ax3, ax4]
 for i, metric in enumerate(set(metrics_aliases.values())):
@@ -176,9 +172,9 @@ fig.savefig('../results/extra_omics_models.pdf', bbox_extra_artists=(lgd,), bbox
 dl_vs_ml_models = ['DL', 'ElasticNet', 'LinearSVR', 'Nystroem+LinearSVR', 'Random Forest',
 				   'XGBoost', 'LGBM']
 ml_results['RMSE'] = ml_results['mean_squared_error'].apply(np.sqrt)
-ml_results = pd.concat([ml_results, dl_results[dl_results['model'] == 'expr (DGI) + drug (ECFP4)']], axis=0)
+ml_results = pd.concat([ml_results, dl_results[dl_results['model'] == '$expr_{DGI} + drugs_{ECFP4}$']], axis=0)
 print(ml_results)
-ml_results['model'].replace(to_replace='expr (DGI) + drug (ECFP4)', value='DL', inplace=True)
+ml_results['model'].replace(to_replace='$expr_{DGI} + drugs_{ECFP4}$', value='DL', inplace=True)
 print(ml_results)
 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(16, 16))
 ax = [ax1, ax2, ax3, ax4]
